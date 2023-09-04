b36cfa09f4b6611638c8dc1abe1213a2fe8b12c6
Geeta Krishna <75007299+ramswami123@users.noreply.github.com>
2023-09-04 15:59:01 +0530

    Update sql_ ref&_commands.sql

--- sql_ ref&_commands.sql
+++ sql_ ref&_commands.sql
@@ -3,3 +3,5 @@ Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from 
 commad : SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[aeiou]';
 
 Regexp:https://www.geeksforgeeks.org/mysql-regular-expressions-regexp/
+
+limit : https://www.w3schools.com/mysql/mysql_limit.asp
