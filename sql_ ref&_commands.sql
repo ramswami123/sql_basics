f43b78a7d546108b0ad2a9ce40e4c966ebefd923
Geeta Krishna <75007299+ramswami123@users.noreply.github.com>
2023-09-06 09:15:03 +0530

    Update sql_ ref&_commands.sql

--- sql_ ref&_commands.sql
+++ sql_ ref&_commands.sql
@@ -5,3 +5,5 @@ commad : SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[aeiou]';
 Regexp:https://www.geeksforgeeks.org/mysql-regular-expressions-regexp/
 
 limit : https://www.w3schools.com/mysql/mysql_limit.asp
+
+right : https://www.w3schools.com/sql/func_sqlserver_right.asp
