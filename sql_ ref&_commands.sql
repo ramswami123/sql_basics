f02abf2def574c0949fc7113d96c30cbaf57c4bb
Geeta Krishna <75007299+ramswami123@users.noreply.github.com>
2023-09-21 16:53:14 +0530

    Update sql_ ref&_commands.sql

--- sql_ ref&_commands.sql
+++ sql_ ref&_commands.sql
@@ -7,3 +7,5 @@ Regexp:https://www.geeksforgeeks.org/mysql-regular-expressions-regexp/
 limit : https://www.w3schools.com/mysql/mysql_limit.asp
 
 right : https://www.w3schools.com/sql/func_sqlserver_right.asp
+
+(IF-ELSE IN SQL)=>https://www.w3schools.com/sql/sql_case.asp
